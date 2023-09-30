ba2ce516b61e216f117c091c092a3bd89c9135f7
BRANDON09716 <heng.2.zhang@kcl.ac.uk>
2023-09-30 19:02:06 +0100

    prime number

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
     list = []
+    candidate = 2
+
+    while len(list) < number_of_primes:
+        is_prime = True
+
+        for divisor in range(2, int(candidate ** 0.5) + 1):
+            if candidate % divisor == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            list.append(candidate)
+
+        candidate += 1
+
     return list
+
